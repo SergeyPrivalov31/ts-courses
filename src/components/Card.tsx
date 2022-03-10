278a9fd8c43c82b645044bbe0e9aad8f2c71f058
privalovsa <privalovsa@it.mos.ru>
2022-03-10 12:23:32 +0300

    передать простые значения

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,26 @@
 import React, {FC} from 'react'
 
+export enum CardVariant { //перечисление enum, в котором описывам что мы ожидаем на входе в этот пропс
+  outlined = 'outlined',
+  primary = 'primary'
+} 
+
 interface CardProps {
   width?: string;
   height?: string;
+  variant: CardVariant;
 }
 
 const Card: FC<CardProps> =
   ({
-    width, height, children
+    width, height, children, variant
   }) => {
     return (
-      <div style={{ width, height, border: '1px solid lightGray', borderRadius: '10px' }}>
+      <div style={{
+        width, height,
+        border: variant === CardVariant.outlined ? '1px solid lightGray' : 'none',
+        backgroundColor: variant === CardVariant.primary ? 'lightGray' : '',
+        borderRadius: '10px' }}>
         {children}
       </div>
     )
