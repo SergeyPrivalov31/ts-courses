94a199abbf2148b3fc0acdccf07d65341ef6c926
privalovsa <privalovsa@it.mos.ru>
2022-03-10 16:30:53 +0300

    useRef типизация

--- src/components/EventsExample.tsx
+++ src/components/EventsExample.tsx
@@ -1,15 +1,16 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useRef, useState } from 'react'
 
 const EventsExample: FC = () => {
     const [value, setValue] = useState<string>('');
     const [isDrag, setIsDrag] = useState<boolean>(false);
+    const inputRef= useRef<HTMLInputElement>(null)
 
     const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         setValue(e.target.value)
     }
 
     const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-        console.log(value)
+        console.log(inputRef.current?.value) //current может быть null, поэтому делаем необязательным ==> /current?/
     }
 
     const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
@@ -34,7 +35,8 @@ const EventsExample: FC = () => {
 
   return (
     <div>
-        <input type='text' value={value} onChange={changeHandler}/>
+        <input type='text' value={value} onChange={changeHandler} placeholder='Управляемый'/>
+        <input ref={inputRef} type='text' placeholder='Неуправляемый'/>
         <button onClick={clickHandler}>Выполнить!</button>
         <div onDrag={dragHandler} draggable style={{width: '200px', height: '200px', background: 'red', marginTop: '15px'}}></div>
         <div
